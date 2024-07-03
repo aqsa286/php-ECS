a44006130e042d328f61d5b1d8709b303248230d
Aqsa Farooq <96003822+aqsa286@users.noreply.github.com>
2024-07-03 19:10:20 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- index.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dummy PHP Application</title>
+</head>
+<body>
+    <h1>Hello, Docker PHP Application!</h1>
+    <p>This is a dummy PHP application running inside a Docker container.</p>
+    <p>PHP Version: <?php echo phpversion(); ?></p>
+</body>
+</html>
